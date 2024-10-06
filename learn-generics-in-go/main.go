8c827ba74dca0a29c606565681b072a706d025fd
arturbaccarin <baccarin.artur@gmail.com>
2024-10-06 09:39:55 -0300

    continuing the video about generics

--- learn-generics-in-go/main.go
+++ learn-generics-in-go/main.go
@@ -1,4 +1,5 @@
 // https://youtu.be/fpLz-SRZ2ho
+// 28:40
 package main
 
 import "fmt"
@@ -6,6 +7,55 @@ import "fmt"
 type Substractable interface {
 	int | int32 | int64 | float32 | float64 | uint | uint32 | uint64
 }
+
+func Subtract[V Substractable](a, b V) V {
+	return a - b
+}
+
+type Moveable[S Substractable] interface {
+	Move(Substractable)
+}
+
+func Move[V Moveable, S Substractable](v V, distance, meters S) {
+	v.Move(meters)
+	return Subtract(distance, meters)
+}
+
+type Person struct {
+	Name string
+}
+
+func (p Person) Move(meters Substractable) {
+	fmt.Printf("%s moved %d meters\n", p.Name, meters)
+}
+
+type Car struct {
+	Name string
+}
+
+func (c Car) Move(meters Substractable) {
+	fmt.Printf("%s moved %d meters\n", c.Name, meters)
+}
+
+func main() {
+	p := Person{Name: "John"}
+	c := Car{Name: "Ferrari"}
+
+	Move(p, 10)
+	Move(c, 10)
+}
+
+/*
+// ~int any date type that derives from int is accepted
+
+type MyOwnInteger int
+
+// Results is a SLICE of results that are Subtractable
+type Results[T Substractable] []T
+
+type Results2[T interface{}] []T
+
+type ResultsC[T comparable] []T
 
 func main() {
 	var a int = 20
@@ -17,6 +67,9 @@ func main() {
 	var e uint = 20
 	var f uint = 10
 
+	var g MyOwnInteger = 20
+	var h MyOwnInteger = 10
+
 	result := Subtract(a, b)
 
 	// We need to cast data type into int here
@@ -24,26 +77,40 @@ func main() {
 
 	resultUint := Subtract[uint](e, f)
 
+	resultMyOwnInteger := Subtract(g, h)
+
 	// Will return 10
 	fmt.Println("Result: ", result)
 	// Will return 10 - Which is wrong
 	fmt.Println("ResultFloat: ", resultFloat)
 
 	fmt.Println("ResultUint: ", resultUint)
+
+	var resultStorage Results[int] // we need to use the type parameter
+
+	resultStorage = append(resultStorage, result, int(resultFloat))
+
+	var resultStorage2 Results[float32] // you can't pass interface substractable
+	resultStorage2 = append(resultStorage2, resultFloat)
+
+	var resultStorage Results2[any]
+	resultStorage = append(resultStorage, result, resultFloat, resultUint, resultMyOwnInteger)
 }
 
 // Subtraction will subtract two numbers
 // [V int] - type constraint, need to be a interface or multiple type
 // int | float32: anonymous interface
-/*
+
 func Subtract[V int | float32](a, b V) V {
 	return a - b
 }
-*/
+
 
 func Subtract[V Substractable](a, b V) V {
 	return a - b
 }
+
+*/
 
 /*
 func main() {
