cf006c9160ccac76c1e527f7c7f06c917f31afef
arturbaccarin <baccarin.artur@gmail.com>
2024-10-09 07:47:52 -0300

    start about attributes

--- structured-logging-slog/slogtest/main.go
+++ structured-logging-slog/slogtest/main.go
@@ -14,8 +14,13 @@ func main() {
 
 	logger := slog.New(logHandler)
 
-	logger.Debug("debug level")
-	logger.Info("test level") // default log is info
-	logger.Warn("warn level")
-	logger.Error("error level")
+	// logger.Debug("What's the meaning of life?", "answer", 42) // smelly code if you have a lot of keys
+	logger.Debug("What's the meaning of life?", slog.Int("answer", 42)) // smelly code if you have a lot of keys
+
+	/*
+		logger.Debug("debug level")
+		logger.Info("test level") // default log is info
+		logger.Warn("warn level")
+		logger.Error("error level")
+	*/
 }
