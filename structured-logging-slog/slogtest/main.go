3e99c79aeadf01c3610a50cc3d90a85af6fd700b
arturbaccarin <baccarin.artur@gmail.com>
2024-10-10 06:46:49 -0300

    a little more about slug

--- structured-logging-slog/slogtest/main.go
+++ structured-logging-slog/slogtest/main.go
@@ -6,16 +6,26 @@ import (
 	"os"
 )
 
+// 20min
 func main() {
-	logHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	logHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{ // NewTextHandler
 		Level:     slog.LevelDebug,
 		AddSource: true,
+	}).WithAttrs([]slog.Attr{
+		slog.Int("answer", 42), // applications informations, versions
 	})
 
 	logger := slog.New(logHandler)
 
+	logger.Debug("Best Pokemon Rating",
+		slog.Group("vodes",
+			slog.Int("Pikachu", 40), // system information inside system group
+			slog.Int("Mew", 22),
+		),
+	)
+
 	// logger.Debug("What's the meaning of life?", "answer", 42) // smelly code if you have a lot of keys
-	logger.Debug("What's the meaning of life?", slog.Int("answer", 42)) // smelly code if you have a lot of keys
+	// logger.Debug("What's the meaning of life?", slog.Int("answer", 42)) // smelly code if you have a lot of keys
 
 	/*
 		logger.Debug("debug level")
